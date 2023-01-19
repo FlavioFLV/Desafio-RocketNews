4af7808ca5cf8d346edd25237f7d38e6d64df4d0
Flávio Prado Da Cunha <409.992.158-17@AEISSP.LOCAL>
2023-01-18 22:47:11 -0300

    Feita leitura do DB e renderizando na pagina

--- src/controllers/EmailController.js
+++ src/controllers/EmailController.js
@@ -1,10 +1,30 @@
+const fs = require('fs');
+
+const readDbFile = () => {
+    const db = fs.readFileSync('./src/database/db.json', 'utf-8')
+    return JSON.parse(db)
+}
+
+const writeDbFile = (content) => {
+    const updateFile = JSON.stringify(content);
+    return fs.writeFileSync("./src/database/db.json", updateFile, "utf-8")
+}
+
+
+
 module.exports = {
     
     read: function(req, res, next) {
+
+        let pageContent = readDbFile();
+
         res.render("listEmails", {
             title: "RocketNews | Lista de emails",
             cssFile: "listEmails",
+            emails: pageContent,
             }
         )
     },
+
+
 }
