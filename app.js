84035e444e3391e6d6fd63320b57c1c919e92e81
Flávio Prado Da Cunha <409.992.158-17@AEISSP.LOCAL>
2023-01-18 22:32:47 -0300

    Criado rota para page listEmails

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./src/routes/indexRouter');
+var emailRouter = require('./src/routes/emailRouter');
 
 var app = express();
 
@@ -19,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/emails', emailRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
