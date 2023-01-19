a2dc76cf3a94c2bbb196dc71a6c2edc46051abbe
Flávio Prado Da Cunha <409.992.158-17@AEISSP.LOCAL>
2023-01-19 07:10:16 -0300

    Criada paginação para a page listEmails

--- src/controllers/EmailController.js
+++ src/controllers/EmailController.js
@@ -16,15 +16,26 @@ module.exports = {
     
     read: function(req, res, next) {
 
-        let pageContent = readDbFile();
+        const currentData = readDbFile();
+        let limit = 10;
+        let page = req.params.page;
+
+        let totalPages = Math.ceil(currentData.length / limit)
+
+        let pageContent;
+        if (page == 1) {            
+            pageContent = currentData.slice(page - 1, limit)
+        } else {
+            pageContent = currentData.slice(((page - 1) * limit), (limit * page))
+        }
 
         res.render("listEmails", {
             title: "RocketNews | Lista de emails",
             cssFile: "listEmails",
             emails: pageContent,
-            }
-        )
+            currentPage: page,
+            totalPages: totalPages,
+        })
     },
-
 
 }
--- src/routes/emailRouter.js
+++ src/routes/emailRouter.js
@@ -4,5 +4,6 @@ var router = express.Router();
 const EmailController = require('../controllers/EmailController');
 
 router.get('/', EmailController.read)
+router.get('/page/:page', EmailController.read);
 
 module.exports = router;
